53805ec5bccb3bea25f183970a8f2193b8b5dca2
Firdiansyah57 <firdiansyah.fh@gmail.com>
2026-03-04 14:32:05 +0700

    cp3 (team b)

--- resources/views/visitor/pages/gallery.blade.php
+++ resources/views/visitor/pages/gallery.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-center pb-4">
             <div class="col-md-12 heading-section text-center ftco-animate">
-                <span class="subheading">Our Blog</span>
+                <span class="subheading">Gambar</span>
                 <h2 class="mb-4">Recent Post</h2>
             </div>
         </div>
